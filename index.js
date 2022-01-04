96250304006c7c5d8b67fc117818904afd2c35f0
amanji <akiff.manji@gmail.com>
2022-01-04 07:10:45 -0800

    Use v4 endpoints for Topic and Credential Set

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ var fields = {
   _inputs: {},
   get defined() {
     return Object.keys(this._inputs)
-      .filter(function (k) {
-        return this._inputs[k];
-      }.bind(this))
-      .map(function (k) {
-        return this._inputs[k];
-      }.bind(this));
+      .filter(
+        function (k) {
+          return this._inputs[k];
+        }.bind(this)
+      )
+      .map(
+        function (k) {
+          return this._inputs[k];
+        }.bind(this)
+      );
   },
   set defined(_inputs) {
     this._inputs = _inputs;
   },
   find: function (k) {
     return this._inputs[k];
-  }
+  },
 };
 
 load(); // Simulates a window onload event
@@ -31,7 +35,7 @@ function load() {
     type: document.querySelector('#type'),
     registryNumber: document.querySelector('#registryNumber'),
     businessNumber: document.querySelector('#businessNumber'),
-  }
+  };
   init();
 }
 
@@ -42,89 +46,111 @@ function init() {
     $(orgNameInput).autocomplete({
       source: function (request, response) {
         $.ajax({
-          url: "https://orgbook.gov.bc.ca/api/v3/search/autocomplete",
+          url: 'https://orgbook.gov.bc.ca/api/v3/search/autocomplete',
           data: {
             q: request.term,
             inactive: 'false',
             revoked: 'false',
-            latest: 'true'
+            latest: 'true',
           },
           success: function (data) {
             var results = data.total ? data.results : [];
             response(results);
-          }
+          },
         });
       },
       minLength: 2,
       select: function (event, ui) {
         clearFields();
         getTopic(ui.item);
-      }
+      },
     });
   } catch (e) {
     console.error('Unable to initialize autocomplete', e);
   }
 }
 
-function getTopic(data) {
+function getTopic(selected) {
   $.ajax({
-    url: "https://orgbook.gov.bc.ca/api/v3/search/topic",
+    url: 'https://orgbook.gov.bc.ca/api/v4/search/topic',
     data: {
-      name: data.topic_source_id
+      q: selected.topic_source_id,
     },
     beforeSend: function () {
       disableFields();
-    }
-  }).done(function (response) {
-    var topicCredential = response.total && response.results.find(function (result) {
-      return result.topic.source_id === data.topic_source_id;
+    },
+  })
+    .done(function (response) {
+      var topic =
+        response.total &&
+        response.results.find(function (_topic) {
+          return _topic.source_id === selected.topic_source_id;
+        });
+      getTopicCredentials(topic || null);
+    })
+    .fail(function (e) {
+      console.error('Unable to get topic', e);
+    })
+    .always(function () {
+      enableFields();
     });
-    getTopicCredentials(topicCredential && topicCredential.topic && topicCredential.topic.id || null);
-  }).fail(function (e) {
-    console.error('Unable to get topic', e);
-  }).always(function () {
-    enableFields();
-  });
 }
 
-function getTopicCredentials(topicId) {
-  if (!topicId) return;
+function getTopicCredentials(topic) {
+  if (!(topic && topic.id)) return;
   $.ajax({
-    url: "https://orgbook.gov.bc.ca/api/v3/search/credential",
-    data: {
-      topic_id: topicId
-    },
+    url:
+      'https://orgbook.gov.bc.ca/api/v4/topic/' + topic.id + '/credential-set',
     beforeSend: function () {
       disableFields();
-    }
-  }).done(function (response) {
-    var credentials = response.total &&
-      response.results &&
-      response.results.filter(function (credential) {
-        return credential.credential_type.description === 'registration.registries.ca' ||
-          credential.credential_type.description === 'business_number.registries.ca';
-      });
+    },
+  })
+    .done(function (response) {
+      var latestValidCredentials =
+        response &&
+        response.length &&
+        response
+          .reduce(function (_latest, _set) {
+            return _latest.concat(
+              _set.credentials.filter(function (credential) {
+                return credential.id === _set.latest_credential_id;
+              })
+            );
+          }, [])
+          .filter(function (credential) {
+            return !credential.revoked;
+          });
 
-    updateFields(credentials);
-  }).fail(function (e) {
-    console.error('Unable to get topic credentials', e);
-  }).always(function () {
-    enableFields();
-  });
+      updateFields(topic, latestValidCredentials);
+    })
+    .fail(function (e) {
+      console.error('Unable to get topic credentials', e);
+    })
+    .always(function () {
+      enableFields();
+    });
 }
 
-function updateFields(credentials) {
+function updateFields(topic, credentials) {
   const orgTypeInput = fields.find('type');
   const orgRNInput = fields.find('registryNumber');
   const orgBNInput = fields.find('businessNumber');
 
   var regCred = credentials.find(function (credential) {
-    return credential.credential_type.description === 'registration.registries.ca'
+    return (
+      credential.credential_type.description === 'registration.registries.ca'
+    );
   });
 
   var bnCred = credentials.find(function (credential) {
-    return credential.credential_type.description === 'business_number.registries.ca'
+    return (
+      credential.credential_type.description === 'business_number.registries.ca'
+    );
   });
+
+  if (topic && orgRNInput) {
+    orgRNInput.value = topic.source_id || '';
+  }
 
   if (regCred && orgRNInput) {
     var orgTypeAttribute = regCred.attributes.find(function (attribute) {
@@ -133,35 +159,28 @@ function updateFields(credentials) {
     orgTypeInput.value = types[orgTypeAttribute.value];
   }
 
-  if (regCred && orgRNInput) {
-    orgRNInput.value = regCred.topic.source_id || '';
-  }
-
   if (bnCred && orgBNInput) {
     const bnAttr = bnCred.attributes.find(function (attribute) {
       return attribute.type === 'business_number';
-    })
-    orgBNInput.value = bnAttr && bnAttr.value || '';
+    });
+    orgBNInput.value = (bnAttr && bnAttr.value) || '';
   }
 }
 
 function clearFields() {
-  fields.defined
-    .forEach(function (field) {
-      field.value = '';
-    });
+  fields.defined.forEach(function (field) {
+    field.value = '';
+  });
 }
 
 function disableFields() {
-  fields.defined
-    .forEach(function (field) {
-      field.setAttribute('disabled', 'true');
-    });
+  fields.defined.forEach(function (field) {
+    field.setAttribute('disabled', 'true');
+  });
 }
 
 function enableFields() {
-  fields.defined
-    .forEach(function (field) {
-      field.removeAttribute('disabled');
-    });
+  fields.defined.forEach(function (field) {
+    field.removeAttribute('disabled');
+  });
 }
